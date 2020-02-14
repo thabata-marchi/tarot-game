3544d1760a8724364efc171d2ec4e0beac0ce183
Thabata Marchi <thabatamarchi@Mac-mini-de-Thabata.local>
2020-02-14 09:23:58 -0300

    Primeiro passo: fiz a comunicação com a api, verificando se o status retornava 200, se sim, setava no state de cardsTarot e pathImg os dados da api, se não, retornava o erro. Após a comunicação com a api, segui o passo da primeira especificação: 'Tela de apresentação exibindo todas as cartas com seu conteúdo visível', dessa forma, mostrei as imagens das cartas de Tarot na tela.

---
+++ src/components/CardTable/index.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react';
+import api from '../../services/api';
+
+const CardTable = () => {
+  const [ cardsTarot, setCardsTarot ] = useState([]);
+  const [ pathImg, setPathImg] = useState("");
+  
+  useEffect( () => {
+    api.ShowCards()
+    .then(response => {
+      if( response.status === 200 ){
+        setCardsTarot([...response.data.cards, response.data]);
+        setPathImg(response.data.imagesUrl);
+      }
+  })
+    .catch( error => console.log(error) )
+  }, [cardsTarot])
+
+  return(
+    <>    
+      <div className="container">
+        <div className="row">
+          {
+            cardsTarot.map(({image}, index) => {
+              return (
+                <div className="col-md-2 col-sm-4 col-xs-12 mt-2 mb-3" key={index}>
+                  <img src={pathImg + image} alt="Imagens" />
+                </div>
+              )
+            })
+
+          }
+        </div>
+      </div>
+    </>
+  ) 
+
+}
+
+export default CardTable;
